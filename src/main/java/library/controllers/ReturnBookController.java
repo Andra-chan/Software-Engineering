9a7b84d9dca8481837737aa78e10f3845b0ca40d
Mihaila Andra <mandrax2001@gmail.com>
2022-05-07 20:25:35 +0300

    Faza 3

--- src/main/java/library/controllers/ReturnBookController.java
+++ src/main/java/library/controllers/ReturnBookController.java
@@ -53,7 +53,7 @@ public class ReturnBookController {
         }
         catch(Exception ex){
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("A problem appeared!");
+            alergit t.setTitle("A problem appeared!");
             alert.setHeaderText("");
             alert.setContentText(ex.getMessage());
             alert.show();
